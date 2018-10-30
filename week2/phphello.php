c51a8198674a0036960dd9510f3ee76cb666c592
Patma saa <ratma.saa@gmail.com>
2018-10-30 10:32:03 +0700

    ส่งครั้งที่1

---
+++ week2/phphello.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Hello PHP World</title>
+    </head>
+    <body>
+       <h1>TEST PHP CODE</h1>
+        <?PHP
+           echo "Hello PHP World";
+    ?>
+     </body>
+</html>
